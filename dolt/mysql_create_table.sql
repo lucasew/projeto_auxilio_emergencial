507a7da9dadfbd65234081d553ab26ff6a2f9ad8
lucasew <lucas59356@gmail.com>
2020-12-15 10:45:37 -0300

    more tweaks
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- dolt/mysql_create_table.sql
+++ dolt/mysql_create_table.sql
@@ -1,3 +1,9 @@
 CREATE TABLE auxilio (id int auto_increment, mes int not null, ibge int not null, nome text not null, parcela int not null, obs text, valor int not null, primary key(id) );
 
 CREATE INDEX auxilio_nome on auxilio (nome);
+CREATE INDEX auxilio_nome_mes on auxilio (nome, mes);
+CREATE INDEX auxilio_ibge on auxilio (ibge);
+
+CREATE VIEW qt_beneficiarios_mes AS
+    SELECT mes, count(mes) FROM auxilio GROUP BY mes;
+
